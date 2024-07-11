c5377f71edfdb668078457c866f6a8f04cb09b7f
Graham Christensen <graham@grahamc.com>
2024-07-10 22:24:16 -0400

    Add schemas for nixos configs

--- flake.nix
+++ flake.nix
@@ -3,9 +3,12 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.1.tar.gz";
+    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
   };
 
   outputs = { nixpkgs, ... }: {
+    schemas = inputs.flake-schemas.schemas;
+
     nixosConfigurations.turner = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
