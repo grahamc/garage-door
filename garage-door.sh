a3e43f1643d5a8caff7310957ba848aa7bbc379f
Graham Christensen <graham@grahamc.com>
2024-07-11 20:00:01 -0400

    fixup gpioset for new nixos

--- garage-door.sh
+++ garage-door.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-if [ "${1:-}" == "outer" ]; then
+if [ "${1:-}" == "outer" ] || [ "${1:-}" == "right" ]; then
   gpio=13
 else
   gpio=19
 fi
 
-gpioset --mode=time --sec=1 pinctrl-bcm2835 "$gpio"=1
+gpioset --toggle 250ms,0 --chip gpiochip0 --strict "$gpio"=1
